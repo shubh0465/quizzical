bfaa819a13b68b009ab10a1a808b14fb2775a1c6
shubh0465 <55250438+shubh0465@users.noreply.github.com>
2023-02-07 10:29:41 +0530

    intro page done

---
+++ src/Components/Intro.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Intro() {
+  return (
+    <div className='intro-page-container'>
+        <h1 className='title-heading'>Quizzical</h1>
+        <p className='description'>Test your skills here</p>
+        <button className='btn'>Start Quiz</button>
+    </div>
+  )
+}
